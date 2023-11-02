180e2df908a4a5121f7f692a509af9d54ff82358
226211 <508800596@qq.com>
2023-11-02 14:43:53 +0800

    l

--- first experiment/Calculator.h
+++ first experiment/Calculator.h
@@ -1,103 +1,109 @@
+#pragma once
 #include <iostream>
 #include<stdlib.h>
 using namespace std;
 #include "LinkStack.h"
 class Calculator
 {
-    private:
-        static bool Operator(char op);//判断字符是否为运算符,当设为静态函数就属于类 可以直接调用而不用创建对象
-        static char Precede(char op1, char op2);//比较运算符优先级
-        static double Operate(double a, char op, double b);//计算
-        static void GetTwoOperands(Linkstack<double> &opnd, double &a, double &b);//从栈中取出两个操作数
-        
-    public:
-        Calculator();
-        ~Calculator();
-        void run();
+private:
+    static bool Operator(char op);//判断字符是否为运算符,当设为静态函数就属于类 可以直接调用而不用创建对象
+    static char Precede(char op1, char op2);//比较运算符优先级
+    static double Operate(double a, char op, double b);//计算
+    static void GetTwoOperands(Linkstack<double>& opnd, double& a, double& b);//从栈中取出两个操作数
+
+public:
+    Calculator() {};
+    ~Calculator() {};
+    void run();
 };
 bool Calculator::Operator(char op)
 {
     switch (op)
     {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '(':
-        case ')':
-            return true;
-        default:
-            return false;
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '(':
+    case ')':
+        return true;
+    default:
+        return false;
     }
 }
 char Calculator::Precede(char op1, char op2)
 {
     switch (op1)
     {
+    case '+':
+    case '-':
+        switch (op2)
+        {
         case '+':
         case '-':
-            switch (op2)
-            {
-                case '+':
-                case '-':
-                case ')':
-                case '*':
-                    return '>';
-                case '/':
-                case '(':
-                    return '<';
-            }
+        case ')':
+        case '*':
+            return '>';
+        case '/':
+        case '(':
+            return '<';
+        }
+        break;
+    case '*':
+    case '/':
+        switch (op2)
+        {
+        case '+':
+        case '-':
         case '*':
         case '/':
-            switch (op2)
-            {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case ')':
-                    return '>';
-                case '(':
-                    return '<';
-            }
-        case '(':
-            switch (op2)
-            {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '(':
-                    return '<';
-                case ')':
-                    return '=';
-            }
         case ')':
-            switch (op2)
-            {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case ')':
-                    return '>';
-                case '(':
-                    return 'E';
-            }
+            return '>';
+        case '(':
+            return '<';
+        }
+        break;
+    case '(':
+        switch (op2)
+        {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '(':
+            return '<';
+        case ')':
+            return '=';
+        }
+        break;
+    case ')':
+        switch (op2)
+        {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case ')':
+            return '>';
+        case '(':
+            return 'E';
+        }
+        break;
+    case '=':
+        switch (op2)
+        {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '(':
+            return '<';
+        case ')':
+            return 'E';
         case '=':
-            switch (op2)
-            {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '(':
-                    return '<';
-                case ')':
-                    return 'E';
-                case '=':
-                    return '='; 
-            }
+            return '=';
+        }
+        break;
     }
     return 'E';
 }
@@ -105,18 +111,18 @@ double Calculator::Operate(double a, char op, double b)
 {
     switch (op)
     {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;
-        case '/': 
-            return a / b;
+    case '+':
+        return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        return a / b;
     }
     return 0;
 }
-void  Calculator:: GetTwoOperands(Linkstack<double> &opnd, double &a, double &b)
+void Calculator::GetTwoOperands(Linkstack<double>& opnd, double& a, double& b)
 {
     opnd.Pop(b);
     opnd.Pop(a);
@@ -125,12 +131,12 @@ void Calculator::run()
 {
     Linkstack<char> optr;
     Linkstack<double> opnd;
-    char ch ='\0',e='\0';
+    char ch = '\0', e = '\0';
     optr.Push('=');
-    while(ch != '=' || e != '=')
+    while (ch != '=' || e != '=')
     {
         cin >> ch;
-        if(!Operator(ch))
+        if (!Operator(ch))
         {
             cin.putback(ch);//将ch放回输入流
             double x;
@@ -140,28 +146,28 @@ void Calculator::run()
         }
         else
         {
-             e = '=';
+            e = '=';
             switch (Precede(e, ch))
             {
-               
-                case '<':
-                    optr.Push(ch);//如果e<ch,则将ch入栈
-                    cin >> ch;
-                    break;
-                case '=':
-                    optr.Pop(ch);//如果e=ch,则直接丢弃ch 只有（）才会出现这种情况
-                    cin >> ch;
-                    break;
-                case '>':
-                    char op;
-                    optr.Pop(op);//将栈顶元素弹出 并进行计算 只有小于才会计算
-                    double a, b;
-                    GetTwoOperands(opnd, a, b);
-                    opnd.Push(Operate(a, op, b));
-                    break;
-                case 'E':
-                    cout << "Error!" << endl;
-                    return;//出错，跳出函数
+
+            case '<':
+                optr.Push(ch);//如果e<ch,则将ch入栈
+                cin >> ch;
+                break;
+            case '=':
+                optr.Pop(ch);//如果e=ch,则直接丢弃ch 只有（）才会出现这种情况
+                cin >> ch;
+                break;
+            case '>':
+                char op;
+                optr.Pop(op);//将栈顶元素弹出 并进行计算 只有小于才会计算
+                double a, b;
+                GetTwoOperands(opnd, a, b);
+                opnd.Push(Operate(a, op, b));
+                break;
+            case 'E':
+                cout << "Error!" << endl;
+                return;//出错，跳出函数
             }
         }
     }
--- first experiment/main.cpp
+++ first experiment/main.cpp
@@ -1,10 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-#include"Calculator.h"
+#include "Calculator.h"
+#include <windows.h>
 int main()
 {
     Calculator cal;
     cal.run();
-    
+    system("pause");
     return 0;
 }
